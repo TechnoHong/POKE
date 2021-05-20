116142460e52d6cd43b03358e9db43269b947252
TechnoHong <tjrwns51@naver.com>
2021-05-20 17:32:57 +0900

    heool

--- Poke/app/src/main/java/com/example/poke/BarcodeScanActivity.java
+++ Poke/app/src/main/java/com/example/poke/BarcodeScanActivity.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 
+<<<<<<< Updated upstream
 
 public class BarcodeScanActivity extends AppCompatActivity{
     @Override
@@ -16,3 +17,29 @@ public class BarcodeScanActivity extends AppCompatActivity{
         new IntentIntegrator(this).initiateScan();
     }
 };
+=======
+public class BarcodeScanActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode);
+
+        new IntentIntegrator(this).initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                // todo
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                // todo
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
+>>>>>>> Stashed changes
